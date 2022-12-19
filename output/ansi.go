2ff0447bfeb8387f85ec725889b9efce686c7840
Anthony Mesakh <anthonyme1211@gmail.com>
2022-12-19 14:03:39 +0700

    separate output implementation

---
+++ output/ansi.go
@@ -0,0 +1,18 @@
+package output
+
+import "os"
+
+type OutputANSIStdOut struct {
+	f *os.File
+}
+
+func (o *OutputANSIStdOut) Write(output []byte) {
+	o.f.Write([]byte("\033[0;0H"))
+	o.f.Write(output)
+}
+
+func NewStdOutput() Output {
+	return &OutputANSIStdOut{
+		f: os.Stdout,
+	}
+}
